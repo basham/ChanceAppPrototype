c8aff40d9fec9601ab30c37663e47798b0375f42
Chris Basham <chris@bash.am>
2012-06-06 11:37:16 -0400

    Crank effect and dice spinning.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
+/*
+ * https://github.com/eightmedia/hammer.js
+ * http://ricostacruz.com/jquery.transit/
+ */
+
 $(document).ready(function() {
 
-	$('html').bind('tapone', roll);
+	//$('html').bind('tapone', drop);
+	//$('html').bind('swipeone', roll);
 	
 	var dice = [];
 	
@@ -8,9 +14,40 @@ $(document).ready(function() {
 		dice.push( new Dice($(this), 6) );
 	})
 	
-	function roll() {
+	$('html').hammer({ prevent_default:true })
+	.bind('drag', function(e) {
+		
+		var maxDistance = 560; // diagonal of 320x460
+		
+		var distance = e.distance;
+		distance = distance > maxDistance ? maxDistance : distance;
+		
+		$.each(dice, function(index, die) {
+			die.crank( distance / maxDistance );
+		});
+	})
+	.bind('dragend tap', function() {
+		$.each(dice, function(index, die) {
+			die.spin();
+		});
+	});
+	
+	function roll(a, b) {
+
+		var maxVelocity = 50;
+		var maxDuration = 1000;
+		var maxDistance = 560; // diagonal of 320x460
+		
+		var duration = b.duration;
+		duration = duration > maxDuration ? maxDuration : duration;
+		var distance = b.delta[0].moved;
+		distance = distance > maxDistance ? maxDistance : distance;
+
+		var velocity = maxVelocity * ( .5 * ( 1 - duration / maxDuration ) + .5 * distance / maxDistance );
+		console.log(duration + ' ' + distance + ' ' + velocity);
+		
 		$.each(dice, function(index, value) {
-			value.roll();
+			value.spin();
 		});
 	}
 	
@@ -27,28 +64,56 @@ function Dice($target, sides) {
 	this.$target = $target;
 	this.sides = sides;
 	this.velocity = 0; // degrees rotation per frame
-	this.degrees = 0;
+	this.degrees = 0; // 0-359
+	this.primer = 0; // 0.0-1.0
 	
-	this.roll = function() {
+	this.crank = function(value) {
+		value = value > 1 ? 1 : ( value < 0 ? 0 : value );
+		this.primer = value;
+		var scale = (1 - value) * .5 + .5;
+		this.$target.css({ scale: scale });
+	};
 	
-		this.velocity = rand(20, 10);	
+	this.release = function() {
+		if( this.primer == 0 )
+			return;
+		var _this = this;
+		//this.$target.css({ scale: 1 });
+		//return;
+		this.$target.transition({
+			scale: 1,
+			opacity: 1,
+			complete: function() {
+				_this.primer = 0;
+			}
+		}, 100);
+	};
+	
+	this.spin = function() {
+		var maxVelocity = 50;
+		this.velocity = this.primer > 0 ? this.primer * maxVelocity : rand(maxVelocity, 5);
+		this.release();
 		this.update();
-		
 	};
 	
 	this.update = function() {
 		
-		if( this.velocity == 0 )
+		if( this.velocity <= 1 ) {
+			this.velocity = 0;
+			this.degrees = Math.floor(this.degrees);
 			return;
-		
-		this.velocity *= .99;
-		this.velocity = this.velocity < 1 ? 0 : this.velocity;
-		
+		}
+		/*
+		var s = this.$target.css('scale');
+		if( s < 1 ) {
+			s *= 1.1;
+			s = s > 1 ? 1 : s;
+			this.$target.css({ scale: s });
+		}
+		*/
+		this.velocity *= 1 - Math.random() / 25; // Deceleration jitter
 		this.degrees += this.velocity;
-		this.degrees = Math.floor(this.degrees);
 		this.degrees %= 360;
-
-		console.log(this.velocity);
 		
 		var v = this.value();
 		this.$target.text(v).removeClass('s1 s2 s3 s4 s5 s6').addClass('s' + v);
