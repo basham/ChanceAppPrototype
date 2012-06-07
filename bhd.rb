8b09b0844ae5b0efe45ebbda143fa537026216fc
Chris Basham <chris@bash.am>
2012-06-07 14:14:34 -0400

    Re-referencing the manifest from the old /manifest URI to try to fix Mobile Safari caching issues.

--- bhd.rb
+++ bhd.rb
@@ -6,6 +6,11 @@ class BitHouseDesign < Sinatra::Base
   get '/' do
     send_file File.join('public', 'index.html')
   end
+  
+  get '/manifest' do
+    headers 'Content-Type' => 'text/cache-manifest'
+    send_file File.join('public', 'cache.manifest')
+  end
 
   not_found do
     redirect '/'
